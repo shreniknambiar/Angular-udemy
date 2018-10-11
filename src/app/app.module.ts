e79e444705c7e39120723c947979cb3d4063d8fc
Shrenik Nambiar (COMMUNICATIONS) <SH20026666@wipro.com>
2018-10-11 17:48:45 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ServerComponent } from './server/server.component'
+import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
